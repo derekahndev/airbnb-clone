68f806003f8449e9205382374130257ebedeff56
derekahndev <derekahndev@gmail.com>
2020-07-03 04:23:25 +0900

    seed_rooms part Three

--- rooms/management/commands/seed_amenities.py
+++ rooms/management/commands/seed_amenities.py
@@ -4,7 +4,7 @@ from rooms.models import Amenity
 
 class Command(BaseCommand):
 
-    help = ""
+    help = "This command creates amenities"
 
     def handle(self, *args, **options):
         amenities = [
