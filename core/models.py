352de1070ffe700521fb48494649b5c7f4bb0712
derekahndev <derekahndev@gmail.com>
2020-07-06 03:47:46 +0900

    get_page vs page

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ class TimeStampedModel(models.Model):
     """ Time Stamped Model """
 
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField()
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
--- rooms/models.py
+++ rooms/models.py
@@ -4,7 +4,6 @@ from core import models as core_models
 
 
 class AbstractItem(core_models.TimeStampedModel):
-
     """ Abstract Item """
 
     name = models.CharField(max_length=80)
@@ -17,40 +16,37 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
-
-    """ RoomType Object Definition """
+    """ RoomType Model Definition """
 
     class Meta:
         verbose_name = "Room Type"
 
 
 class Amenity(AbstractItem):
-
-    """ Amenity Object Definition """
+    """ Amenity Model Definition """
 
     class Meta:
         verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
+    """ Facility Model Definition """
 
-    """ Facility Object Definition """
+    pass
 
     class Meta:
         verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
-
-    """ HouseRule Object Definition """
+    """ HouseRule Model Definition """
 
     class Meta:
         verbose_name = "House Rule"
 
 
 class Photo(core_models.TimeStampedModel):
-
-    """ Photo Object Definition """
+    """ Photo Model Definition """
 
     caption = models.CharField(max_length=80)
     file = models.ImageField(upload_to="room_photos")
@@ -61,7 +57,6 @@ class Photo(core_models.TimeStampedModel):
 
 
 class Room(core_models.TimeStampedModel):
-
     """ Room Model Definition """
 
     name = models.CharField(max_length=140)
@@ -78,24 +73,20 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(
-        "users.User", related_name="rooms", on_delete=models.CASCADE
+        "users.User", related_name="rooms", on_delete=models.CASCADE,
     )
     room_type = models.ForeignKey(
-        "RoomType",
-        related_name="rooms",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True,
     )
     amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
     facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
     house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):
-        self.city = str.capitalize(self.city)
         return self.name
 
     def save(self, *args, **kwargs):
+        self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
 
     def total_rating(self):
